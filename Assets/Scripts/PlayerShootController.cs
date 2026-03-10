2618bdef9b370462eb1342c5b302bccf78a06d3d
student-mel <melonym1101@gmail.com>
2026-03-10 21:04:34 +1100

    Object Pooling

--- Assets/Scripts/PlayerShootController.cs
+++ Assets/Scripts/PlayerShootController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Settings.Input;
 using UnityEngine;
 
@@ -6,17 +7,23 @@ public class PlayerShootController : MonoBehaviour
 {
     public InputReader inputReader;
 
-    [Header("Shoot Settings")] 
-    public GameObject bulletPrefab;
-    [SerializeField] private float fireRate = 5, projectileSpeed = 2;
+    [Header("Projectile Settings")] 
+    [SerializeField] private float fireRate = 5;
+    [SerializeField] private float bulletSpeed = 2;
     [SerializeField] private Transform shootOrigin;
+    
+    [Header("Projectile Pooling")]
+    public GameObject bulletPrefab;
+    [SerializeField] private int poolSize = 30;
+    [SerializeField] private Queue<GameObject> bulletPool = new Queue<GameObject>();
     
     [Header("Debug")]
     public bool debug;
-    
+
     private void OnEnable()
     {
         inputReader.ShootEvent += ShootEvent;
+        CreatePool();
     }
 
     private void OnDisable()
@@ -26,9 +33,32 @@ public class PlayerShootController : MonoBehaviour
     
     private void ShootEvent()
     {
-        Instantiate(bulletPrefab, shootOrigin.position, Quaternion.identity, shootOrigin);
+        SpawnBullet();
     }
 
+    #region Object Pooling
+
+    private void CreatePool()
+    {
+        for (int i = 0; i < poolSize; i++)
+        {
+            bulletPool.Enqueue(Instantiate(bulletPrefab));
+        }
+    }
+
+    private void SpawnBullet()
+    {
+        GameObject bullet = bulletPool.Dequeue();
+        bullet.transform.position = shootOrigin.position;
+        bullet.SetActive(true);
+    }
+
+    public void EnqueueBullet(GameObject bullet)
+    {
+        bulletPool.Enqueue(bullet);
+    }
+    #endregion
+  
     private void DebugMessage(string message)
     {
         if (debug)
