150292ca109b28ca6380ae107dec5887e92b6fca
cat-sun-1234 <chentongsun1320@gmail.com>
2026-03-15 21:52:40 +1100

    main menu fully functional. level select activating a sub-menu. and enabled the pause menu buttons on completing the last level (keep players from being stuck there)

--- Assets/Scripts/LevelProgressionManager.cs
+++ Assets/Scripts/LevelProgressionManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine.InputSystem; // Required for new Input System
+using UnityEngine.UI;
 
 public class LevelProgressionManager : MonoBehaviour
 {
@@ -10,6 +11,26 @@ public class LevelProgressionManager : MonoBehaviour
 
     private bool levelComplete = false;
 
+    [Header("Pause Menu Buttons")]
+    [SerializeField] private Button retryButton;
+    [SerializeField] private Button menuButton;
+    [SerializeField] private Button exitButton;
+
+    void Awake()
+    {
+        if (retryButton == null)
+        {
+            retryButton = GameObject.Find("retryButton").GetComponent<Button>(); // Find the Button component for the retry button in the scene
+        }
+        if (menuButton == null)
+        {
+            menuButton = GameObject.Find("menuButton").GetComponent<Button>(); // Find the Button component for the retry button in the scene
+        }
+        if (exitButton == null)
+        {
+            exitButton = GameObject.Find("exitButton").GetComponent<Button>(); // Find the Button component for the retry button in the scene
+        }
+    }
     void Start()
     {
         if (continueText != null)
@@ -37,6 +58,13 @@ public class LevelProgressionManager : MonoBehaviour
         {
             continueText.enabled = true;
             continueText.text = "Press ENTER to continue";
+        }
+        if (SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCountInBuildSettings-1)
+        {
+            continueText.text = "Game Complete";
+            retryButton.gameObject.SetActive(true); // Enable the retry button
+            menuButton.gameObject.SetActive(true); // Enable the menu button
+            exitButton.gameObject.SetActive(true); // Enable the exit button
         }
     }
 
--- Assets/Scripts/MainMenuHandler.cs
+++ Assets/Scripts/MainMenuHandler.cs
@@ -1,7 +1,50 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuHandler : MonoBehaviour
 {
+    [SerializeField] private Button tutLevelButton;
+    [SerializeField] private Button levelSelectButton;
+    [SerializeField] private Button level1Button;
+    [SerializeField] private Button level2Button;
+    [SerializeField] private Button level3Button;
+    [SerializeField] private Button exitLevelSelectButton;
+    [SerializeField] private Button exitButton;
+
+    void Start()
+    {
+        mainMenuActivate(true);
+        LevelSelectMenuActivate(false);
+    }
+    public void LevelSelectClicked()
+    {
+        mainMenuActivate(false);
+        LevelSelectMenuActivate(true);
+    }
+
+    public void exitLevelSelect()
+    {
+        mainMenuActivate(true);
+        LevelSelectMenuActivate(false);
+    }
+
+    public void tutLevelClicked()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1); 
+    }
+
+    public void Level1Clicked()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+    }
+    public void Level2Clicked()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(3);
+    }
+    public void Level3Clicked()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(4);
+    }
     public void QuitButtonClick()
     {
         Application.Quit();
@@ -9,5 +52,20 @@ public class MainMenuHandler : MonoBehaviour
         #if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
         #endif
+    }
+
+    public void mainMenuActivate (bool activate)
+    {
+        tutLevelButton.gameObject.SetActive(activate);
+        levelSelectButton.gameObject.SetActive(activate);
+        exitButton.gameObject.SetActive(activate);
+    }
+
+    public void LevelSelectMenuActivate (bool activate)
+    {
+        level1Button.gameObject.SetActive(activate);
+        level2Button.gameObject.SetActive(activate);
+        level3Button.gameObject.SetActive(activate);
+        exitLevelSelectButton.gameObject.SetActive(activate);
     }
 }
